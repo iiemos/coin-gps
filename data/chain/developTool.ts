b481a32529ffa6865580874b6b540032db089351
halley <halley@wikifx.com>
2023-04-25 18:22:15 +0800

    完善信息

---
+++ data/chain/developTool.ts
@@ -0,0 +1,16 @@
+export default {
+  data: [
+    {
+      Name: "ChainTool",
+      Icon: "",
+      Type: ["Docs"],
+      OfficialUrl: "https://chaintool.tech/abi",
+      Twitter: "https://twitter.com/NUpchain",
+      Telegram: "",
+      Discord: "",
+      Facebook: "",
+      Team: [],
+      Desc: `ABI 可视化调用工具[1]（英文称为 ABI Caller ）会自动根据 ABI 生成可调用按钮，而不需要手动编写和发送交易。类似 Etherscan 在合约开源之后，提供的读写合约的功能。不过有时候，我们在开发时，合约一开始是不开源的，这个时候使用 Chaintool ABI 可视化调用工具就比较方便了。在 ABI 可视化调用工具，很多时候，智能合约开发者需要编写相应的前端或脚本来手动编写和发送交易，这会给开发者带来更多的心智负担，尤其是智能合约开发者可能并不熟悉 JavaScript 等语言`,
+    },
+  ],
+};
--- data/chain/tool.ts
+++ data/chain/tool.ts
@@ -1,11 +1,16 @@
 export default {
   data: [
     {
-      Name: 'Google Authenticator',
-      Icon: '/img/audit/GoogleAuthenticator.svg',
-      Type: ['Tool'],
-      OfficialUrl: 'https://googleauthenticator.net/',
+      Name: "Google Authenticator",
+      Icon: "/img/audit/GoogleAuthenticator.svg",
+      Type: ["Tool"],
+      OfficialUrl: "https://googleauthenticator.net/",
+      Twitter: "",
+      Telegram: "",
+      Discord: "",
+      Facebook: "",
+      Team: [],
       desc: `Google 身份验证器会在您的手机上生成两步验证码。`,
     },
-  ]
-}
+  ],
+};
